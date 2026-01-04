0d101ec6a225677651838cfa9da78278a3c677e1
Gamatek <mathieu.bontemps3.0@gmail.com>
2026-01-04 18:15:20 +0100

    refactor(twitch-auto-claim): remove debug logging and simplify code
    - Remove all console.log statements
    
    - Inline createObservers function
    
    - Add @grant none directive
    
    - Bump version to 1.0.1

--- twitch-auto-claim/twitch-auto-claim.user.js
+++ twitch-auto-claim/twitch-auto-claim.user.js
@@ -2,7 +2,7 @@
 // @name         AutoClaimTwitch
 // @description  Will automatically click the button for claiming channel points on Twitch.
 // @icon         https://raw.githubusercontent.com/Gamatek/gamatek-userscripts/refs/heads/main/twitch-auto-claim/icon128.png
-// @version      1.0.0
+// @version      1.0.1
 
 // @author       Gamatek
 // @namespace    https://github.com/Gamatek
@@ -11,6 +11,7 @@
 
 // @match        https://*.twitch.tv/*
 // @run-at       document-start
+// @grant        none
 // ==/UserScript==
 
 (function() {
@@ -22,26 +23,16 @@
     let lastPointsSummarySection;
     let lastClick = 0;
 
-    function log(...args) {
-        console.log(
-            "%c TwitchAutoClaim %c",
-            "background: #00db84; color: #000000;", null,
-            ...args
-        );
-    };
-
     function clickBonusButton() {
         if (!summaryElems[0]) return;
         const bonusBtn = summaryElems[0].querySelector(".community-points-summary > *:nth-child(2) button");
         if (bonusBtn && (Date.now() - lastClick) > (1000 + Math.random() * 1000)) {
             lastClick = Date.now();
-            log("Claiming bonus", bonusBtn);
             bonusBtn.click();
         };
     };
 
     function observeBonus() {
-        log("Creating mutation obvserver on points summary section", summaryElems[0]);
         lastPointsSummarySection = summaryElems[0];
         clickBonusButton();
         observers.push(
@@ -51,17 +42,13 @@
         );
     };
 
-    function createObservers() {
-        if (summaryElems[0]) observeBonus();
-        log("Creating document mutation observer");
-        observers.push(
-            new MutationObserver(() => {
-                if (summaryElems[0] && summaryElems[0] !== lastPointsSummarySection) {
-                    observeBonus();
-                };
-            }).observe(document.body, { subtree: true, childList: true }),
-        );
-    };
+    if (summaryElems[0]) observeBonus();
 
-    createObservers();
+    observers.push(
+        new MutationObserver(() => {
+            if (summaryElems[0] && summaryElems[0] !== lastPointsSummarySection) {
+                observeBonus();
+            };
+        }).observe(document.body, { subtree: true, childList: true }),
+    );
 })();
